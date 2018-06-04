a11a386b75b4bf8d5024b7d5eb66cd67e0d32d5f
Jordan Walker <jorwalk@gmail.com>
2018-06-04 08:37:11 -0500

    include link to folder in image read

--- examples/analog-gauge-reader/analog_gauge_reader.py
+++ examples/analog-gauge-reader/analog_gauge_reader.py
@@ -1,11 +1,10 @@
-'''  
+'''
 Copyright (c) 2017 Intel Corporation.
 Licensed under the MIT license. See LICENSE file in the project root for full license information.
 '''
 
 import cv2
 import numpy as np
-#import paho.mqtt.client as mqtt
 import time
 
 def avg_circles(circles, b):
@@ -23,7 +22,6 @@ def avg_circles(circles, b):
     return avg_x, avg_y, avg_r
 
 def dist_2_pts(x1, y1, x2, y2):
-    #print np.sqrt((x2-x1)^2+(y2-y1)^2)
     return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 def calibrate_gauge(gauge_number, file_type):
@@ -38,14 +36,9 @@ def calibrate_gauge(gauge_number, file_type):
         and the units (as a string).
     '''
 
-    img = cv2.imread('gauge-%s.%s' %(gauge_number, file_type))
+    img = cv2.imread('./images/gauge-%s.%s' %(gauge_number, file_type))
     height, width = img.shape[:2]
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #convert to gray
-    #gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    # gray = cv2.medianBlur(gray, 5)
-
-    #for testing, output gray image
-    #cv2.imwrite('gauge-%s-bw.%s' %(gauge_number, file_type),gray)
 
     #detect circles
     #restricting the search from 35-48% of the possible radii gives fairly good results across different samples.  Remember that
@@ -58,10 +51,6 @@ def calibrate_gauge(gauge_number, file_type):
     #draw center and circle
     cv2.circle(img, (x, y), r, (0, 0, 255), 3, cv2.LINE_AA)  # draw circle
     cv2.circle(img, (x, y), 2, (0, 255, 0), 3, cv2.LINE_AA)  # draw center of circle
-
-    #for testing, output circles on image
-    #cv2.imwrite('gauge-%s-circles.%s' % (gauge_number, file_type), img)
-
 
     #for calibration, plot lines from center going out at every 10 degrees and add marker
     #for i from 0 to 36 (every 10 deg)
@@ -103,12 +92,12 @@ def calibrate_gauge(gauge_number, file_type):
     cv2.imwrite('gauge-%s-calibration.%s' % (gauge_number, file_type), img)
 
     #get user input on min, max, values, and units
-    print 'gauge number: %s' %gauge_number
-    min_angle = raw_input('Min angle (lowest possible angle of dial) - in degrees: ') #the lowest possible angle
-    max_angle = raw_input('Max angle (highest possible angle) - in degrees: ') #highest possible angle
-    min_value = raw_input('Min value: ') #usually zero
-    max_value = raw_input('Max value: ') #maximum reading of the gauge
-    units = raw_input('Enter units: ')
+    print ('gauge number: %s' %gauge_number)
+    min_angle = input('Min angle (lowest possible angle of dial) - in degrees: ') #the lowest possible angle
+    max_angle = input('Max angle (highest possible angle) - in degrees: ') #highest possible angle
+    min_value = input('Min value: ') #usually zero
+    max_value = input('Max value: ') #maximum reading of the gauge
+    units = input('Enter units: ')
 
     #for testing purposes: hardcode and comment out raw_inputs above
     # min_angle = 45
@@ -120,35 +109,15 @@ def calibrate_gauge(gauge_number, file_type):
     return min_angle, max_angle, min_value, max_value, units, x, y, r
 
 def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, gauge_number, file_type):
-
-    #for testing purposes
-    #img = cv2.imread('gauge-%s.%s' % (gauge_number, file_type))
-
     gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Set threshold and maxValue
     thresh = 175
     maxValue = 255
 
-    # for testing purposes, found cv2.THRESH_BINARY_INV to perform the best
-    # th, dst1 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_BINARY);
-    # th, dst2 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_BINARY_INV);
-    # th, dst3 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_TRUNC);
-    # th, dst4 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_TOZERO);
-    # th, dst5 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_TOZERO_INV);
-    # cv2.imwrite('gauge-%s-dst1.%s' % (gauge_number, file_type), dst1)
-    # cv2.imwrite('gauge-%s-dst2.%s' % (gauge_number, file_type), dst2)
-    # cv2.imwrite('gauge-%s-dst3.%s' % (gauge_number, file_type), dst3)
-    # cv2.imwrite('gauge-%s-dst4.%s' % (gauge_number, file_type), dst4)
-    # cv2.imwrite('gauge-%s-dst5.%s' % (gauge_number, file_type), dst5)
-
     # apply thresholding which helps for finding lines
     th, dst2 = cv2.threshold(gray2, thresh, maxValue, cv2.THRESH_BINARY_INV);
 
-    # found Hough Lines generally performs better without Canny / blurring, though there were a couple exceptions where it would only work with Canny / blurring
-    #dst2 = cv2.medianBlur(dst2, 5)
-    #dst2 = cv2.Canny(dst2, 50, 150)
-    #dst2 = cv2.GaussianBlur(dst2, (5, 5), 0)
 
     # for testing, show image after thresholding
     cv2.imwrite('gauge-%s-tempdst2.%s' % (gauge_number, file_type), dst2)
@@ -158,15 +127,8 @@ def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, 
     maxLineGap = 0
     lines = cv2.HoughLinesP(image=dst2, rho=3, theta=np.pi / 180, threshold=100,minLineLength=minLineLength, maxLineGap=0)  # rho is set to 3 to detect more lines, easier to get more then filter them out later
 
-    #for testing purposes, show all found lines
-    # for i in range(0, len(lines)):
-    #   for x1, y1, x2, y2 in lines[i]:
-    #      cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    #      cv2.imwrite('gauge-%s-lines-test.%s' %(gauge_number, file_type), img)
-
     # remove all lines outside a given radius
     final_line_list = []
-    #print "radius: %s" %r
 
     diff1LowerBound = 0.15 #diff1LowerBound and diff1UpperBound determine how close the line should be from the center
     diff1UpperBound = 0.25
@@ -187,14 +149,6 @@ def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, 
                 # add to final list
                 final_line_list.append([x1, y1, x2, y2])
 
-    #testing only, show all lines after filtering
-    # for i in range(0,len(final_line_list)):
-    #     x1 = final_line_list[i][0]
-    #     y1 = final_line_list[i][1]
-    #     x2 = final_line_list[i][2]
-    #     y2 = final_line_list[i][3]
-    #     cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
     # assumes the first line is the best one
     x1 = final_line_list[0][0]
     y1 = final_line_list[0][1]
@@ -203,7 +157,6 @@ def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, 
     cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     #for testing purposes, show the line overlayed on the original image
-    #cv2.imwrite('gauge-1-test.jpg', img)
     cv2.imwrite('gauge-%s-lines-2.%s' % (gauge_number, file_type), img)
 
     #find the farthest point from the center to be what is used to determine the angle
@@ -217,12 +170,6 @@ def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, 
         y_angle = y - y2
     # take the arc tan of y/x to find the angle
     res = np.arctan(np.divide(float(y_angle), float(x_angle)))
-    #np.rad2deg(res) #coverts to degrees
-
-    # print x_angle
-    # print y_angle
-    # print res
-    # print np.rad2deg(res)
 
     #these were determined by trial and error
     res = np.rad2deg(res)
@@ -234,8 +181,6 @@ def get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, 
         final_angle = 90 - res
     if x_angle > 0 and y_angle < 0:  #in quadrant IV
         final_angle = 270 - res
-
-    #print final_angle
 
     old_min = float(min_angle)
     old_max = float(max_angle)
@@ -258,10 +203,9 @@ def main():
     min_angle, max_angle, min_value, max_value, units, x, y, r = calibrate_gauge(gauge_number, file_type)
 
     #feed an image (or frame) to get the current value, based on the calibration, by default uses same image as calibration
-    img = cv2.imread('gauge-%s.%s' % (gauge_number, file_type))
+    img = cv2.imread('./images/gauge-%s.%s' % (gauge_number, file_type))
     val = get_current_value(img, min_angle, max_angle, min_value, max_value, x, y, r, gauge_number, file_type)
-    print "Current reading: %s %s" %(val, units)
+    print ("Current reading: %s %s" %(val, units))
 
 if __name__=='__main__':
     main()
-   	
